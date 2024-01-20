1bfa659c8197abef6dff2a67d0c9ea4ac3bfd2e3
Kalkan <gjtqy985@gmail.com>
2024-01-20 15:58:01 +0900

    名称の記載/テーブルの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,6 +15,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+      @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :information, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,22 +5,26 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee_status
   belongs_to :prefecture
   belongs_to :scheduled_delivery
+  belongs_to :user
+  has_one :purchase_history
+  
   has_one_attached :image
   validates :image, presence: { message: "can't be blank" }
+
   #空の投稿を保存できないようにする
   validates :name, :information, presence: true
 
   #ジャンルの選択が「---」の時は保存できないようにする
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :sales_status_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
-  
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
+
   validates :price, presence: true, numericality: {
     only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid", allow_blank: true
   }
-
-  belongs_to :user
 end
 
