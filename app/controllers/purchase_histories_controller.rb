9767453c33f87deeedf1650289f80eb40945caeb
Kalkan <gjtqy985@gmail.com>
2024-01-31 17:43:05 +0900

    直打ちの制限

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -1,17 +1,17 @@
 class PurchaseHistoriesController < ApplicationController
-  before_action :authenticate_user!
-  # before_action :contributor_confirmation, only: :index
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  before_action :contributor_confirmation, only: [:index, :create]
+
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-    @item = Item.find(params[:item_id])
     @order_purchase_history = OrderPurchaseHistory.new
-    if @order_purchase_history.present?
-      redirect_to root_path
-    end
+    # if @purchase_history.present?
+    #   redirect_to root_path
+    # end
   end
   def create
     @order_purchase_history = OrderPurchaseHistory.new(purchase_history_params)
-    @item = Item.find(params[:item_id])
     if @order_purchase_history.valid?
       pay_item
       @order_purchase_history.save
@@ -34,10 +34,11 @@ class PurchaseHistoriesController < ApplicationController
       currency: 'jpy'
     )
   end
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+  def contributor_confirmation
+    redirect_to root_path if current_user.id == @item.user.id || @item.purchase_history.present?
+  end
 
-  # def contributor_confirmation
-  #   if @order_purchase_history.present?
-  #     redirect_to root_path
-  #   end
-  # end
 end
